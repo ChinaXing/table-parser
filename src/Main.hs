b61b0a5d2765e93cfca672c9be61304af2e97f52
陈云星 <chenyunxing@youzan.com>
2017-10-19 21:40:10 +0800

    fix encoding, set to utf8

--- src/Main.hs
+++ src/Main.hs
@@ -1,8 +1,11 @@
 module Main where
 
 import TableParser
+import System.IO
 
 main :: IO ()
 main = do
+  hSetEncoding stdin utf8
+  hSetEncoding stdout utf8
   contents <- getContents
   putStrLn $ parseJson contents
